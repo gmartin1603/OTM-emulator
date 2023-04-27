0c4ed85d5e0f5758d5dc96a360f04f7ed20115f6
George <duramaxer228@gmail.com>
2023-04-26 19:37:48 -0500

    Added buildArchive to fsApp

--- functions/index.js
+++ functions/index.js
@@ -337,6 +337,176 @@ fsApp.post('/deleteOldPosts', cors({origin: URLs.local}), async (req,res) => {
   return res.json(JSON.stringify({message:`Deleted ${deleted.length} postings`})).send()
 })
 // --------------------------------------------------------- //
+fsApp.post('/buildArchive', cors({origin: URLs.local}), async (req,res) => {
+  const body = JSON.parse(req.body)
+  let rota = {};
+  let jobs = [];
+  let dept = body.dept
+  const today = new Date(body.start);
+  today.setHours(7)
+  // console.log(`Today => ${today}`)
+
+  const findWeek = (today, start, rotaLength) => {
+    let timeSinceStart = today.getTime() - start;
+    let weeksSince = timeSinceStart / (24 * 60 * 60 * 1000 * 7);
+    let weekNumber = Math.ceil(weeksSince % rotaLength);
+
+    return weekNumber;
+  }
+  const sortShifts = (shiftObj) => {
+    const keys = Object.keys(shiftObj)
+    let shiftArr = []
+    for (const prop in keys) {
+      shiftArr.push(shiftObj[keys[prop]])
+    }
+    shiftArr.sort((a, b) => {
+      if (a.order < b.order) {
+          return -1
+      }
+      if (a.order > b.order) {
+          return 1
+      }
+      return 0
+    })
+    return shiftArr
+  }
+
+  const buildRows = (shift, posts, week) => {
+    let arr = []
+    jobs.length > 0 &&
+    // loop through all jobs
+    jobs.map((job) => {
+      let archiveRow = structuredClone(job)
+      // if shift is true in job
+      if (job[shift.id]){
+        let show = true
+        // set color
+        let color = shift.color[job.group][0]
+        const prevRow = arr[arr.length - 1]
+        // if previous job exists
+        if (arr.length > 0) {
+          if (prevRow.group === job.group) {
+            if (prevRow.color === shift.color[job.group][0]) {
+              color = shift.color[job.group][1]
+            } else {
+              color = shift.color[job.group][0]
+            }
+          }
+        }
+        archiveRow.data = {
+          group: job.group,
+          label: job.label,
+          color: color,
+          id: job.id,
+          1:'',
+          2:'',
+          3:'',
+          4:'',
+          5:'',
+          6:'',
+          7:''
+        } //mon to sun
+        // if not "misc"
+        if (job.data) {
+          // for each day in the job
+          for (const day in job.data) {
+            // if the job has rotation data for the shift
+            if (job.data[day][shift.id]) {
+              // for each week in the rotation
+              for (const key in job.data[day][shift.id]) {
+                if (key === week.toString()) {
+                  // set the archiveRow.data to the rotation data for the week
+                  archiveRow.data[day] = rota.fields[shift.id][job.group][job.data[day][shift.id][key]]
+                }
+              }
+            }
+          }
+        } else {
+          show = false
+          for (const key in posts) {
+            const post = posts[key]
+            // console.log(post)
+            if (post.shift === shift.id) {
+              if (post.pos === job.id) {
+                // rowPosts[post.date] = post
+                show = true
+                let date = new Date(post.date)
+                switch (date.getDay()) {
+                  case 0:
+                    archiveRow.data[7] =  post.id
+                    break;
+                  default:
+                    archiveRow.data[date.getDay()] = post.id
+                }
+              }
+            }
+          }
+        }
+
+        if (show) {
+          arr.push(archiveRow.data)
+        }
+      }
+    })
+    return arr
+  }
+
+  // Get posts for the week to determine if "misc" row should be shown
+  const posts = await db.collection(`${dept}-posts`)
+  .where('date', '>=', today.getTime())
+  .where('date', '<=', today.getTime() + (7 * (24 * 60 * 60 * 1000)))
+  .get()
+  .then((snapshot) => {
+    let obj = {}
+    snapshot.forEach((doc) => {
+      obj[doc.id] = doc.data()
+      admin.firestore().collection(`${dept}-posts`).doc(doc.id).set({locked: true}, {merge: true})
+      .catch((error) => {
+        console.log(`Error locking post ${doc.id}`, error)
+      })
+    });
+    return obj
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+
+  await db.collection(dept)
+  .orderBy('order')
+  .get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      if (doc.data().id === "rota") {
+        rota = doc.data();
+      } else {
+        jobs.push(doc.data())
+      }
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+
+  let obj = {};
+
+  const week = findWeek(today, rota.start, rota.length);
+  const shifts = sortShifts(rota.shifts)
+
+  shifts.map(shift => {
+    let rows = buildRows(shift, posts, week)
+    obj[shift.id] = {shift: shift, rows: rows}
+  })
+
+  await db.collection(dept).doc('rota').collection('archive').doc(`${today.toDateString()}`).set(obj)
+  .then(() => {
+    console.log(`Doc written to ${dept}/rota/archive/${today.toDateString()}`)
+  })
+  .catch((error) => {
+    console.error('Error writing document: ', error);
+  });
+
+  return res.json(JSON.stringify({message: `${today.toDateString()} successfully archived`})).send()
+})
 
 fsApp.post('/setPost', cors({origin: URLs.local}), async (req,res) => {
   let body = JSON.parse(req.body)
@@ -405,8 +575,193 @@ fsApp.post('/setPost', cors({origin: URLs.local}), async (req,res) => {
         }
       } else {
         // create archive doc
-        return res.json(JSON.stringify({message: "Archive doc not found"})).send()
+        const dept = 'csst'
+        let rota = {};
+        let jobs = [];
+        const today = new Date(body.archive);
+        today.setHours(7)
+        console.log(`Today => ${today}`)
+
+        const findMon = (today) => {
+          //Daylight Savings check
+          const jan = new Date(today.getFullYear(), 0, 1);
+          // console.log(`Daylight Savings => ${today.getTimezoneOffset() < jan.getTimezoneOffset()}`)
+          let day = 24 * 60 * 60 * 1000
+          //  time = today - milliseconds past midnight + 1 hour if today.getTimezoneOffset < jan.getTimezoneOffset
+          let time = (today - ((today.getHours() * 60 * 60 * 1000) + (today.getMinutes() * 60 * 1000) + (today.getSeconds() * 1000) + today.getMilliseconds()))+(today.getTimezoneOffset() < jan.getTimezoneOffset()? (60*60*1000) : 0)
+          let d = today.getDay()
+          if (d === 0) {
+            d = 7
+          }
+          //monday = time - (day of the week * ms in a day) + 1 day in ms
+          let mon = time - (d * day) + day
+
+          return new Date(mon)
+        }
+
+        const monday = findMon(today)
+
+        const findWeek = (today, start, rotaLength) => {
+          let timeSinceStart = today.getTime() - start;
+          let weeksSince = timeSinceStart / (24 * 60 * 60 * 1000 * 7);
+          let weekNumber = Math.ceil(weeksSince % rotaLength);
+
+          return weekNumber;
+        }
+        const sortShifts = (shiftObj) => {
+          const keys = Object.keys(shiftObj)
+          let shiftArr = []
+          for (const prop in keys) {
+            shiftArr.push(shiftObj[keys[prop]])
+          }
+          shiftArr.sort((a, b) => {
+            if (a.order < b.order) {
+                return -1
+            }
+            if (a.order > b.order) {
+                return 1
+            }
+            return 0
+          })
+          return shiftArr
+        }
+
+        const buildRows = (shift, posts, week) => {
+          let arr = []
+          jobs.length > 0 &&
+          // loop through all rows
+          jobs.map((job) => {
+            let archiveRow = structuredClone(job)
+            // if shift is true in job
+            if (job[shift.id]){
+              let show = true
+              // set color
+              let color = shift.color[job.group][0]
+              const prevRow = arr[arr.length - 1]
+              // if previous job exists
+              if (arr.length > 0) {
+                if (prevRow.group === job.group) {
+                  if (prevRow.color === shift.color[job.group][0]) {
+                    color = shift.color[job.group][1]
+                  } else {
+                    color = shift.color[job.group][0]
+                  }
+                }
+              }
+              archiveRow.data = {
+                group: job.group,
+                label: job.label,
+                color: color,
+                id: job.id,
+                1:'',
+                2:'',
+                3:'',
+                4:'',
+                5:'',
+                6:'',
+                7:''
+              } //mon to sun
+              // if not "misc"
+              if (job.data) {
+                // for each day in the job
+                for (const day in job.data) {
+                  // if the job has rotation data for the shift
+                  if (job.data[day][shift.id]) {
+                    // for each week in the rotation
+                    for (const key in job.data[day][shift.id]) {
+                      if (key === week.toString()) {
+                        // set the archiveRow.data to the rotation data for the week
+                        archiveRow.data[day] = rota.fields[shift.id][job.group][job.data[day][shift.id][key]]
+                      }
+                    }
+                  }
+                }
+              } else {
+                show = false
+                for (const key in posts) {
+                  const post = posts[key]
+                  // console.log(post)
+                  if (post.shift === shift.id) {
+                    if (post.pos === job.id) {
+                      // rowPosts[post.date] = post
+                      show = true
+                      let date = new Date(post.date)
+                      switch (date.getDay()) {
+                        case 0:
+                          archiveRow.data[7] =  post.id
+                          break;
+                        default:
+                          archiveRow.data[date.getDay()] = post.id
+                      }
+                    }
+                  }
+                }
+              }
+
+              if (show) {
+                arr.push(archiveRow.data)
+              }
+            }
+          })
+          return arr
+        }
+
+        // Get posts for the week to determine if "misc" row should be shown
+        const posts = await db.collection(`${dept}-posts`)
+        .where('date', '>=', monday.getTime())
+        .where('date', '<=', monday.getTime() + (7 * (24 * 60 * 60 * 1000)))
+        .get()
+        .then((snapshot) => {
+          let obj = {}
+          snapshot.forEach((doc) => {
+            obj[doc.id] = doc.data()
+            admin.firestore().collection(`${dept}-posts`).doc(doc.id).set({locked: true}, {merge: true})
+            .catch((error) => {
+              console.log(`Error locking post ${doc.id}`, error)
+            })
+          });
+          return obj
+        })
+        .catch((err) => {
+          console.log('Error getting documents', err);
+        });
+
+        await db.collection(dept)
+        .orderBy('order')
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            if (doc.data().id === "rota") {
+              rota = doc.data();
+            } else {
+              jobs.push(doc.data())
+            }
+          });
+        })
+        .catch((err) => {
+          console.log('Error getting documents', err);
+        });
+
+        let obj = {};
+
+        const week = findWeek(today, rota.start, rota.length);
+        const shifts = sortShifts(rota.shifts)
+
+        shifts.map(shift => {
+          let rows = buildRows(shift, posts, week)
+          obj[shift.id] = {shift: shift, rows: rows}
+        })
+
+        await db.collection(dept).doc('rota').collection('archive').doc(`${monday.toDateString()}`).set(obj)
+        .then(() => {
+          console.log(`Doc written to ${dept}/rota/archive/${monday.toDateString()}`)
+        })
+        .catch((error) => {
+          console.error('Error writing document: ', error);
+        });
+        return res.json(JSON.stringify({message: "Archive doc created"})).send()
       }
+
       await admin.firestore()
       .collection(body.dept)
       .doc('rota')
@@ -730,6 +1085,7 @@ fsApp.post('/deleteDocField', cors({origin: URLs.local}), async (req, res) => {
 })
 
 exports.fsApp = functions.https.onRequest(fsApp)
+
 //***************** End FsApp ************* */
 
 //***************** Start Pub/Sub ************* */
@@ -741,7 +1097,7 @@ exports.fsApp = functions.https.onRequest(fsApp)
 exports.pubSub = functions.https.onRequest(async (req, res) => {
   const body = JSON.parse(req.body)
   let rota = {};
-  let rows = [];
+  let jobs = [];
   let dept = body.dept
   const today = new Date(body.start);
   today.setHours(7)
@@ -773,60 +1129,51 @@ exports.pubSub = functions.https.onRequest(async (req, res) => {
 
     return weekNumber;
   }
-
-  const sort = (arr) => {
-    arr.sort((a, b) => {
-        if (a.order < b.order) {
-            return -1
-        }
-        if (a.order > b.order) {
-            return 1
-        }
-        // if (a === b)
-        return 0
-    })
-  }
-
   const sortShifts = (shiftObj) => {
     const keys = Object.keys(shiftObj)
     let shiftArr = []
     for (const prop in keys) {
       shiftArr.push(shiftObj[keys[prop]])
     }
-    sort(shiftArr)
+    shiftArr.sort((a, b) => {
+      if (a.order < b.order) {
+          return -1
+      }
+      if (a.order > b.order) {
+          return 1
+      }
+      return 0
+    })
     return shiftArr
   }
 
   const buildRows = (shift, posts, week) => {
-    // const mon = cols[0].label
-    // const sun = cols[6].label
     let arr = []
-    let rowPosts = {}
-    rows.length > 0 &&
-    // loop through all rows
-    rows.map((row, i) => {
-      let archiveRow = structuredClone(row)
-      // if shift is true in row
-      if (row[shift.id]){
+    jobs.length > 0 &&
+    // loop through all jobs
+    jobs.map((job) => {
+      let archiveRow = structuredClone(job)
+      // if shift is true in job
+      if (job[shift.id]){
         let show = true
         // set color
-        let color = shift.color[row.group][0]
+        let color = shift.color[job.group][0]
         const prevRow = arr[arr.length - 1]
-        // if previous row exists
+        // if previous job exists
         if (arr.length > 0) {
-          if (prevRow.group === row.group) {
-            if (prevRow.color === shift.color[row.group][0]) {
-              color = shift.color[row.group][1]
+          if (prevRow.group === job.group) {
+            if (prevRow.color === shift.color[job.group][0]) {
+              color = shift.color[job.group][1]
             } else {
-              color = shift.color[row.group][0]
+              color = shift.color[job.group][0]
             }
           }
         }
         archiveRow.data = {
-          group: row.group,
-          label: row.label,
+          group: job.group,
+          label: job.label,
           color: color,
-          id: row.id,
+          id: job.id,
           1:'',
           2:'',
           3:'',
@@ -836,16 +1183,16 @@ exports.pubSub = functions.https.onRequest(async (req, res) => {
           7:''
         } //mon to sun
         // if not "misc"
-        if (row.data) {
-          // for each day in the row
-          for (const day in row.data) {
-            // if the row has rotation data for the shift
-            if (row.data[day][shift.id]) {
+        if (job.data) {
+          // for each day in the job
+          for (const day in job.data) {
+            // if the job has rotation data for the shift
+            if (job.data[day][shift.id]) {
               // for each week in the rotation
-              for (const key in row.data[day][shift.id]) {
+              for (const key in job.data[day][shift.id]) {
                 if (key === week.toString()) {
                   // set the archiveRow.data to the rotation data for the week
-                  archiveRow.data[day] = rota.fields[shift.id][row.group][row.data[day][shift.id][key]]
+                  archiveRow.data[day] = rota.fields[shift.id][job.group][job.data[day][shift.id][key]]
                 }
               }
             }
@@ -856,7 +1203,7 @@ exports.pubSub = functions.https.onRequest(async (req, res) => {
             const post = posts[key]
             // console.log(post)
             if (post.shift === shift.id) {
-              if (post.pos === row.id) {
+              if (post.pos === job.id) {
                 // rowPosts[post.date] = post
                 show = true
                 let date = new Date(post.date)
@@ -875,15 +1222,15 @@ exports.pubSub = functions.https.onRequest(async (req, res) => {
         if (show) {
           arr.push(archiveRow.data)
         }
-        rowPosts = {}
       }
     })
     return arr
   }
+
   // Get posts for the week to determine if "misc" row should be shown
   const posts = await db.collection(`${dept}-posts`)
-  .where('date', '>=', monday.getTime())
-  .where('date', '<=', monday.getTime() + (7 * (24 * 60 * 60 * 1000)))
+  .where('date', '>=', today.getTime())
+  .where('date', '<=', today.getTime() + (7 * (24 * 60 * 60 * 1000)))
   .get()
   .then((snapshot) => {
     let obj = {}
@@ -908,7 +1255,7 @@ exports.pubSub = functions.https.onRequest(async (req, res) => {
       if (doc.data().id === "rota") {
         rota = doc.data();
       } else {
-        rows.push(doc.data())
+        jobs.push(doc.data())
       }
     });
   })
@@ -934,7 +1281,7 @@ exports.pubSub = functions.https.onRequest(async (req, res) => {
     console.error('Error writing document: ', error);
   });
 
-  res.send(`${monday} Successfully archived`)
+  return res.send(`${today} Successfully archived`)
 })
 
 //***************** End Pub/Sub ************* */
